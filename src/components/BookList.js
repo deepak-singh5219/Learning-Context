22a2e04450d2009b07e2e273c5eda12f14f8ec75
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-16 11:36:11 +0530

    another way of consuming Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,17 +5,27 @@ import { ThemeContext } from '../context/ThemeContext';
 class BookList extends Component {
   static contextType = ThemeContext;
   render() { 
-    console.log(this.context);
-    const {isLightTheme, light, dark} = this.context;
-    const theme = !isLightTheme? light: dark;
+    
     return ( 
-      <div className="book-list" style={{'background':theme.bg}}>
-        <ul style={{'background':theme.ui, 'color':theme.syntax}}>
-          <li>the way of kings</li>
-          <li>the name of the wind</li>
-          <li>the final empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {
+          (context) => {
+    console.log("BookList",context);
+    const {isLightTheme, light, dark} = context;
+    const theme = isLightTheme? light: dark;
+            return (
+              <div className="book-list" style={{'background':theme.bg}}>
+              <ul style={{'background':theme.ui, 'color':theme.syntax}}>
+                <li>the way of kings</li>
+                <li>the name of the wind</li>
+                <li>the final empire</li>
+              </ul>
+            </div>
+            )
+          }
+        }
+      </ThemeContext.Consumer>
+      
     );
   }
 }
