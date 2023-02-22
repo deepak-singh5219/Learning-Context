0db6b0d3dc9b185f6dbaef9d99422b6eb3dbf198
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-22 15:34:36 +0530

    converted book list to functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,51 +1,33 @@
-import React, { Component } from 'react';
+import React, {  useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import { AuthContext } from '../context/AuthContext';
 
+const BookList = () => {
+const {isLightTheme, light, dark} = useContext(ThemeContext);
+const {isAuth} = useContext(AuthContext);
+const theme = isLightTheme? light: dark;
 
-
-class BookList extends Component {
-  render() { 
-    
-    return ( 
-
-      <AuthContext.Consumer>{
-        (authContext) => {
-          return (
-          <ThemeContext.Consumer>
-        {(themeContext) => {
-    const {isAuth} = authContext;
-    const {isLightTheme, light, dark} = themeContext;
-    const theme = isLightTheme? light: dark;
-            return (
-              <>
-              {
-                !isAuth?(
-                  <div>
-                    You are not Logged In!!!
-                  </div>
-                ):(
-                  <div className="book-list" style={{'background':theme.bg}}>
-                <ul style={{'background':theme.ui, 'color':theme.syntax}}>
-                  <li>the way of kings</li>
-                  <li>the name of the wind</li>
-                  <li>the final empire</li>
-                </ul>
-              </div>
-                )
-              }
-              </>
-            )
-          }
-        }
-      </ThemeContext.Consumer>)
-        }
-        }
-      </AuthContext.Consumer>
-      
-    );
-  }
+  return (
+    <>
+    { 
+        !isAuth?(
+          <div>
+            You are not Logged In!!!
+          </div>
+        ):(
+          <div className="book-list" style={{'background':theme.bg}}>
+        <ul style={{'background':theme.ui, 'color':theme.syntax}}>
+          <li>the way of kings</li>
+          <li>the name of the wind</li>
+          <li>the final empire</li>
+        </ul>
+      </div>
+        )
+      }
+    </>
+  )
 }
+
  
 export default BookList;
 
