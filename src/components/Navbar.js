6548c1db618e209608a46dc6aabff990575d66be
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-22 15:38:41 +0530

    converted navbar to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React, {  useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-
-class Navbar extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme? light: dark;
-    return ( 
-      <nav style={{'background':theme.bg}}>
-        <h1 style={{'color':theme.syntax}}>Context App</h1>
-        <ul style={{'background':theme.ui, 'color':theme.syntax}}>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    );
-  }
+const Navbar = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme? light: dark;
+  return (
+    <nav style={{'background':theme.bg}}>
+    <h1 style={{'color':theme.syntax}}>Context App</h1>
+    <ul style={{'background':theme.ui, 'color':theme.syntax}}>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+  )
 }
+
  
 export default Navbar;
